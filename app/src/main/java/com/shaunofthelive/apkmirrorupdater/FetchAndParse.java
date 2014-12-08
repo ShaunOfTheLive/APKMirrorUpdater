5e9a1cd2304c6d61c09ac895571e6de285f68761
Shaun Hill <shaun.hill@gmail.com>
2014-12-08 15:54:31 -0500

    it would probably help to actually add FetchAndParse.java in git

---
+++ app/src/main/java/com/shaunofthelive/apkmirrorupdater/FetchAndParse.java
@@ -0,0 +1,53 @@
+package com.shaunofthelive.apkmirrorupdater;
+
+import android.os.AsyncTask;
+import android.util.Log;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+
+/**
+ * Created by Shaun on 2014-12-08.
+ */
+public class FetchAndParse extends AsyncTask<String, Void, String> {
+    private static final String TAG = "MY_TAG";
+
+    protected String doInBackground(String... urls) {
+        try {
+            Document doc = Jsoup.connect("http://www.apkmirror.com/").get();
+
+            Elements appNameElements = doc.select(".table-row .visible-xs-block:not(.widget_appmanager_recentpostswidget *)");
+            ArrayList<String> appNames = new ArrayList<String>();
+
+            for (Element el : appNameElements) {
+                appNames.add(el.text());
+            }
+            Collections.sort(appNames);
+            for (String appName : appNames) {
+                Log.d(TAG, appName);
+            }
+        } catch(IOException ie) {
+            Log.d(TAG, "EXCEPTION: IOException");
+        }
+
+        return "test";
+    }
+
+    protected void onProgressUpdate(Integer... progress) {
+        //
+    }
+
+    protected void onPreExecute() {
+        Log.d(TAG, "preExecute");
+    }
+
+    protected void onPostExecute(String result) {
+        Log.d(TAG, "postExecute");
+    }
+}
