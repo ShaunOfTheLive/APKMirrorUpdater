4278cc7d07041826dba537373d6a83aec5f9c875
Shaun Hill <shaun.hill@gmail.com>
2014-12-16 13:11:42 -0500

    instantiate RemoteAppsFragment on tab 2

--- app/src/main/java/com/shaunofthelive/apkmirrorupdater/MainActivity.java
+++ app/src/main/java/com/shaunofthelive/apkmirrorupdater/MainActivity.java
@@ -94,10 +94,13 @@ public class MainActivity extends ActionBarActivity
         public Fragment getItem(int position) {
             // getItem is called to instantiate the fragment for the given page.
             // Return a PlaceholderFragment (defined as a static inner class below).
-            if (position > 0) {
-                return PlaceholderFragment.newInstance(position + 1);
-            } else {
-                return new InstalledAppsFragment();
+            switch (position) {
+                case 0:
+                    return new InstalledAppsFragment();
+                case 1:
+                    return new RemoteAppsFragment();
+                case 2:
+                    return PlaceholderFragment.newInstance(position + 1);
             }
         }
 
