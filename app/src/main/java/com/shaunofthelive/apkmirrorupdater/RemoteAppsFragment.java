d4d6c7b68d96e05e696ab17727cf5bc8714e33e2
Shaun Hill <shaun.hill@gmail.com>
2014-12-16 13:02:22 -0500

    moved FetchAndParse to an inner class of RemoteAppsFragment

--- app/src/main/java/com/shaunofthelive/apkmirrorupdater/RemoteAppsFragment.java
+++ app/src/main/java/com/shaunofthelive/apkmirrorupdater/RemoteAppsFragment.java
@@ -1,19 +1,33 @@
 package com.shaunofthelive.apkmirrorupdater;
 
 import android.app.Activity;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.os.AsyncTask;
 import android.os.Bundle;
-import android.app.Fragment;
+import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AbsListView;
 import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.ListAdapter;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
-import com.shaunofthelive.apkmirrorupdater.dummy.DummyContent;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * A fragment representing a list of Items.
@@ -49,8 +63,8 @@ public class RemoteAppsFragment extends Fragment implements AbsListView.OnItemCl
     private ListAdapter mAdapter;
 
     // TODO: Rename and change types of parameters
-    public static RemoteAppsFragment newInstance(String param1, String param2) {
-        RemoteAppsFragment fragment = new RemoteAppsFragment();
+    public static InstalledAppsFragment newInstance(String param1, String param2) {
+        InstalledAppsFragment fragment = new InstalledAppsFragment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -65,6 +79,81 @@ public class RemoteAppsFragment extends Fragment implements AbsListView.OnItemCl
     public RemoteAppsFragment() {
     }
 
+    private class FetchAndParse extends AsyncTask<String, Integer, ArrayList<AppInfo>> {
+        private static final String TAG = "MY_TAG";
+
+        @Override
+        protected ArrayList<AppInfo> doInBackground(String... s) {
+            ArrayList<AppInfo> appList = new ArrayList<AppInfo>();
+
+            try {
+                Document doc = Jsoup.connect("http://www.apkmirror.com/").get();
+
+                Elements appNameElements = doc.select(".table-row .visible-xs-block:not(.widget_appmanager_recentpostswidget *)");
+                ArrayList<String> appNames = new ArrayList<String>();
+
+                for (Element el : appNameElements) {
+                    appNames.add(el.text());
+                }
+
+                Elements versionElements = doc.select(".infoSlide:not(.widget_appmanager_recentpostswidget *)");
+                ArrayList<String> versionStrings = new ArrayList<String>();
+
+                for (Element el : versionElements) {
+                    //Log.d(TAG, "el: " + el);
+                    versionStrings.add(el.text());
+                }
+
+                Pattern versionPattern = Pattern.compile(".*Latest version: (.*) \\((.*)\\) for Android (.*\\+) \\(.*API ([0-9]+)\\).*");
+                String versionName;
+                int versionCode;
+                int minimumApi;
+
+                int i = 0;
+                for (String versionString : versionStrings) {
+                    Matcher m = versionPattern.matcher(versionString); // put element here
+
+                    if (m.matches()) {
+                        versionName = m.group(1);
+                        versionCode = Integer.parseInt(m.group(2));
+                        // no group 3; ignore minimum Android version e.g. 4.0.3+
+                        minimumApi = Integer.parseInt(m.group(4));
+                    } else {
+                        versionName = "ERROR";
+                        versionCode = 0;
+                        minimumApi = 0;
+                    }
+
+                    appList.add(new AppInfo("", appNames.get(i), versionName, versionCode, minimumApi));
+                    i++;
+                }
+
+            } catch(IOException ie) {
+                Log.d(TAG, "EXCEPTION: IOException");
+            }
+
+            return appList;
+        }
+
+        @Override
+        protected void onProgressUpdate(Integer... progress) {
+            //
+        }
+
+        @Override
+        protected void onPreExecute() {
+            Log.d("DEBUG", "preExecute");
+        }
+
+        @Override
+        protected void onPostExecute(ArrayList<AppInfo> appList) {
+            for (AppInfo appInfo : appList) {
+                Log.d("SITE", appInfo.toString());
+            }
+
+        }
+    }
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -74,15 +163,25 @@ public class RemoteAppsFragment extends Fragment implements AbsListView.OnItemCl
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
 
-        // TODO: Change Adapter to display your content
-        mAdapter = new ArrayAdapter<DummyContent.DummyItem>(getActivity(),
-                android.R.layout.simple_list_item_1, android.R.id.text1, DummyContent.ITEMS);
+        //ArrayList<AppInfo> apps = getAllAppInfo();
+
+        new FetchAndParse().execute();
+
+        // Create the adapter to convert the array to views
+        //mAdapter = new AppInfoAdapter(getActivity(), apps);
+
+        //debug log
+//        for (AppInfo appInfo: apps) {
+//            Log.d("INST", "Application name: " + appInfo.getApplicationName());
+//            Log.d("INST", "Version: " + appInfo.getVersionName());
+//        }
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_remote_apps, container, false);
+        View view = inflater.inflate(R.layout.fragment_installed_apps, container, false);
 
         // Set the adapter
         mListView = (AbsListView) view.findViewById(android.R.id.list);
@@ -117,7 +216,12 @@ public class RemoteAppsFragment extends Fragment implements AbsListView.OnItemCl
         if (null != mListener) {
             // Notify the active callbacks interface (the activity, if the
             // fragment is attached to one) that an item has been selected.
-            mListener.onFragmentInteraction(DummyContent.ITEMS.get(position).id);
+            //mListener.onFragmentInteraction(...);
+            AppInfo app = (AppInfo)mAdapter.getItem(position);
+            Toast.makeText(getActivity(), "Version code of "
+                            + app.getApplicationName() + ": "
+                            + app.getVersionCode(),
+                    Toast.LENGTH_LONG).show();
         }
     }
 
